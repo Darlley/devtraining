a444cda19d2a75ab3cc7e330e21bdf225e2367f7
Darlley Brasil de Brito Furtado <darlleybrito@gmail.com>
2022-10-20 17:22:14 -0400

    database include

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      autoLoadModels: true,
+    }),
+    CoursesModule,
+    TweetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
